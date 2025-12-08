b36db5f861134b8925bee0dbd34d472aef918748
Diego Perez <digo@Diegos-MacBook-Pro.local>
2025-12-07 22:14:14 -0600

    Completed Activity 4: Product API with full CRUD operations

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -7,6 +7,7 @@ namespace Persistence
     {
         public DbSet<WeatherForecast> WeatherForecasts { get; set; }
         public string DbPath { get; }
+        public DbSet<Product> Products { get; set; }
 
         public DataContext()
         {
@@ -19,5 +20,7 @@ namespace Persistence
         {
             options.UseSqlite($"Data Source={DbPath}");
         }
+
+
     }
 }
